15102bccd1ae15ce1c74b1237bfbd585c8f3b106
Bruno Santos <c201199@ardentmills.com>
2021-07-14 16:02:50 -0300

    added the singleton

---
+++ DesignPatterns/CreationalPatterns/Singleton/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
---
+++ DesignPatterns/CreationalPatterns/Singleton/ConsoleApp1/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Singleton
+    {
+        // The Singleton's constructor should always be private to prevent
+        // direct construction calls with the `new` operator.
+        private Singleton() { }
+
+        // The Singleton's instance is stored in a static field. There there are
+        // multiple ways to initialize this field, all of them have various pros
+        // and cons. In this example we'll show the simplest of these ways,
+        // which, however, doesn't work really well in multithreaded program.
+        private static Singleton _instance;
+
+        // This is the static method that controls the access to the singleton
+        // instance. On the first run, it creates a singleton object and places
+        // it into the static field. On subsequent runs, it returns the client
+        // existing object stored in the static field.
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        // Finally, any singleton should define some business logic, which can
+        // be executed on its instance.
+        public static string someBusinessLogic()
+        {
+            return "Works";
+        }
+    }
+}
