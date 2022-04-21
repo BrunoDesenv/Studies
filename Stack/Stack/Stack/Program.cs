fa2b967c76dfdf7ed286feac44578cd7043c47fd
Bruno <bru_blink@hotmail.com>
2022-04-21 12:50:32 -0300

    Add project stack

---
+++ Stack/Stack/Stack/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackInt stack = new StackInt();
+
+            string item;
+            string opcao;
+            StackInt.Retorno retorno = new StackInt.Retorno();
+
+            //Initialize the stack
+            stack.INIT();
+
+            //Invoke method TOP for obtain data of node from top of stack
+            retorno = stack.TOP();
+
+            if (retorno.ok)
+                Console.WriteLine("Execução do TOP: " + retorno.item);
+            else
+                Console.WriteLine("Execução do TOP: pilha vazia");
+
+
+            //looop to insert values in the stack
+            do
+            {
+                Console.WriteLine("Digite valor inteiro para dado");
+                item = Console.ReadLine();
+                stack.PUSH(Convert.ToInt32(item));
+
+                Console.WriteLine("Digite 0 para encerrar empilhamento de dados");
+                opcao = Console.ReadLine();
+
+            } while (!opcao.Equals("0"));
+
+            //Invoke method TOP for obtain data of node from top of stack
+            retorno = stack.TOP();
+            if (retorno.ok)
+                Console.WriteLine("Execução do TOP: " + retorno.item);
+
+            //loop for remov all elements of stack
+            do
+            {
+                retorno = stack.POP();
+                if (retorno.ok)
+                    Console.WriteLine("Dados retirado: "  + retorno.item);
+            } while (retorno.ok);
+
+
+        }
+    }
+}
---
+++ Stack/Stack/Stack/StackInt.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class StackInt
+    {
+        //Definition of NO
+        private class NO
+        {
+            public int dado { get; set; }
+            public NO prox { get; set; }
+        }
+
+        //Definition of return from methods POP and TOP
+        public class Retorno
+        {
+            public int item { get; set; }
+            public Boolean ok { get; set; }
+        }
+
+        //Definition of pointer top of stack
+        private static NO topo;
+
+        public void INIT()
+        {
+            topo = null;
+        }
+
+        public bool IsEmpty()
+        {
+            return topo == null;
+        }
+
+        public void PUSH(int item)
+        {
+            NO novo = new NO();
+            novo.dado = item;
+            novo.prox = topo;
+            topo = novo;
+        }
+
+        public Retorno POP()
+        {
+            Retorno saida = new Retorno();
+            if (!IsEmpty())
+            {
+                saida.item = topo.dado;
+                topo = topo.prox;
+                saida.ok = true;
+            }
+            else
+            {
+                saida.ok = false;
+            }
+
+            return saida;
+        }
+
+        public Retorno TOP()
+        {
+            Retorno saida = new Retorno();
+            if (!IsEmpty()) 
+            {
+                saida.item = topo.dado;
+                saida.ok = true;
+            }
+            else
+            {
+                saida.ok = false;
+            }
+
+            return saida;
+        }
+
+    }
+}
